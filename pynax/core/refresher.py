efcc58a5673d8bf32eafc26f26a34f53f8cfcbf1
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-04-22 01:16:28 +0200

    Clean

--- pynax/core/refresher.py
+++
@@ -1,9 +0,0 @@
-class Refresher:
-
-    def __init__(self, figure, mark_list):
-        self.figure = figure
-        for m in mark_list:
-            m.subscribe(self)
-
-    def on_update(self, value):
-        self.figure.canvas.draw()
