d92b9e2cc1d198e39f47fc638e454fb142a3455f
Alexandre Abraham <abraham.alexandre@gmail.com>
2013-04-29 18:32:52 +0200

    New 4D viewer

--- pynax/Viewer4D.py
+++ pynax/Viewer4D.py
@@ -1,48 +1,65 @@
-from . import Figure, create_axes, Mark
-from matplotlib import widgets
+import pylab as pl
+import numpy as np
+
+from . import Mark
+from .view import ImshowView, PlotView
 
 
 def show(*args):
-    # Expand display_options if provided
-    layers = []
-    for arg in args:
-        if isinstance(arg, tuple):
-            layers.append((arg[0], arg[1]))
-        else:
-            layers.append((arg, {}))
+    data = args[0]
 
-    fig = Figure((2, 3))
-    x, y, z, n = create_axes(['x', 'y', 'z', 'n'])
-    mx = Mark(x, 20, {'color': 'r'})
-    my = Mark(y, 20, {'color': 'g'})
-    mz = Mark(z, 20, {'color': 'b'})
-    mn = Mark(n, 20, {'color': 'k'})
+    fig = pl.figure(figsize=(17, 6))
 
-    vx = fig.add((0, 2), layers[0][0], mx, y, z, [mn],
-                 display_options=layers[0][1])
-    vx.add_mark(my)
-    vx.add_mark(mz)
-    vy = fig.add((0, 1), layers[0][0], my, x, z, [mn],
-                 display_options=layers[0][1])
-    vy.add_mark(mx)
-    vy.add_mark(mz)
-    vz = fig.add((0, 0), layers[0][0], mz, x, y, [mn],
-                 display_options=layers[0][1])
-    vz.add_mark(mx)
-    vz.add_mark(my)
+    display_options = {}
+    display_options['interpolation'] = 'nearest'
+    display_options['cmap'] = pl.cm.gray
 
-    def update(val):
-        if int(val) != mn.value:
-            mn.value = int(val)
-            fig.propagate_changes([mn])
+    # Marks
+    mx = Mark(20, {'color': 'r'})
+    my = Mark(20, {'color': 'g'})
+    mz = Mark(20, {'color': 'b'})
+    mn = Mark(0, {'color': 'k'})
 
-    slider_ax = fig.get_subplot((1, 0), (3, 1))
-    slider = widgets.Slider(slider_ax, 'n', 0, layers[0][0].shape[-1] - 1)
-    slider.on_changed(update)
-    """
-    for data, options in layers[1:]:
-        vx.add_layer(data, display_options=options)
-        vy.add_layer(data, display_options=options)
-        vz.add_layer(data, display_options=options)
-    """
-    return fig, slider
+    # Figure axis
+    #ax_x = pl.subplot(231)
+    #ax_y = pl.subplot(232)
+    #ax_z = pl.subplot(233)
+    #ax_n = pl.subplot(212)
+
+    ax_x = fig.add_axes([0.0, 0.2, 0.37, 0.8])
+    ax_y = fig.add_axes([0.38, 0.2, 0.37, 0.8])
+    ax_z = fig.add_axes([0.76, 0.2, 0.23, 0.8])
+    ax_n = fig.add_axes([0.05, 0.05, 0.9, 0.195])
+
+    ax_x.axis('off')
+    ax_y.axis('off')
+    ax_z.axis('off')
+
+    # Views
+
+    vx = ImshowView(ax_x, data, [mx, 'h', '-v', mn], display_options)
+    vx.add_hmark(my)
+    vx.add_vmark(mz)
+
+    vy = ImshowView(ax_y, data, ['h', my, '-v', mn], display_options)
+    vy.add_hmark(mx)
+    vy.add_vmark(mz)
+
+    vz = ImshowView(ax_z, data, ['h', '-v', mz, mn], display_options)
+    vz.add_hmark(mx)
+    vz.add_vmark(my)
+
+    vn = PlotView(ax_n, data, [mx, my, mz, 'h'], {'color': 'gray'})
+    pl.ylim(0, np.max(data))
+    vn.add_hmark(mn)
+
+    ac_display_options = {}
+    ac_display_options['interpolation'] = 'nearest'
+    ac_display_options['cmap'] = pl.cm.autumn
+
+    vx.draw()
+    vy.draw()
+    vz.draw()
+    vn.draw()
+
+    pl.show()
